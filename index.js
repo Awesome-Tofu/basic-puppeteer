2b43b1ce446aaee9d13e66dc1f4dee45296ddc9e
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2024-02-12 21:24:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // write code with puppeteer-core to screenshot and send image using express
 const express = require('express');
 const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;  
+const port = 3000;
 
 app.get('/screenshot', async (req, res) => {
     const weburl = req.query.url;
-    const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH, 
-    });
+    const browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage();
     await page.goto(weburl);
     const screenshotPath = '/tmp/screenshot.png';
